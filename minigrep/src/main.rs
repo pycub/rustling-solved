26db1f0119a70c8c73cc9d91d32a02eb8cdc9b2e
Hossein Sokhandan <iamsokhandan@gmail.com>
2024-06-22 14:14:23 +0330

    Redirect to stdout instead of stdin

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("{err}");
+        eprintln!("{err}");
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         process::exit(1);
     }
 }
